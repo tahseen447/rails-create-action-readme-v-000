11e3a3e75f306fe6872b81fe350f98f76d76808a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-10-19 22:58:39 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   post.title = params[:title]
   post.description = params[:description]
   post.save
-  redirect to post_path(@post)
+  redirect_to post_path(@post)
 end
 
 end
